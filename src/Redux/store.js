0ebc04b6df7f801b95319224858b7aaf03bb208f
codewarsfx <chiderainnocent001@gmail.com>
2022-03-28 14:08:41 +0100

    add redirect file for netlify

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
 
 import { applyMiddleware,compose } from "redux";
+import {persistStore, persistReducer} from 'redux-persist';
 import logger from "redux-logger";
 
 import rootReducer from './root-reducer'
